0c4a378830dffc490129e7b44ca0d81eebb3bb9d
sbx_user1051 <sbx_user1051@169.254.21.47>
2025-08-31 19:13:14 +0000

    Sun Aug 31 19:13:14 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,7 +7,34 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus) -> int:
-        PRICES: Dict[str, int] = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
+        PRICES: Dict[str, int] = {
+            "A": 50,
+            "B": 30,
+            "C": 20,
+            "D": 15,
+            "E": 40,
+            "F": 10,
+            "G": 20,
+            "H": 10,
+            "I": 35,
+            "J": 60,
+            "K": 80,
+            "L": 90,
+            "M": 15,
+            "N": 40,
+            "O": 10,
+            "P": 50,
+            "Q": 30,
+            "R": 50,
+            "S": 30,
+            "T": 20,
+            "U": 40,
+            "V": 50,
+            "W": 20,
+            "X": 90,
+            "Y": 10,
+            "Z": 50,
+        }
         OFFERS: Dict[str, list[tuple[int, int]]] = {
             "A": [(5, 200), (3, 130)],
             "B": [(2, 45)],
@@ -59,5 +86,6 @@ class CheckoutSolution:
                 total += qty * PRICES[item]
 
         return total
+
 
 
